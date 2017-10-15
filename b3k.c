323509f8030a494dd1a54f73975512471f0598a9
twngbm <twngbm@gmail.com>
2017-10-16 00:44:35 +0800

    Disable init loop,add input limit.

--- b3k.c
+++ b3k.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 /* Pictures of zero, largest positive and largest negative numbers. */
-static const char *pat = "┌───┐\n"
-                         "│   │\n"
-                         "└───┘\n"
-                         "├─┴─┤\n"
-                         "┤   ├\n"
-                         "├─┬─┤\n"
-                         "┌┬┬┬┐\n"
-                         "├   ┤\n"
-                         "└┴┴┴┘\n";
+static const char *pat = "┌───┐\n"//0~15
+                         "│   │\n"//16~25
+                         "└───┘\n"//26~41
+                         "├─┴─┤\n"//42~57
+                         "┤   ├\n"//58~67
+                         "├─┬─┤\n"//68~83
+                         "┌┬┬┬┐\n"//84~99
+                         "├   ┤\n"//100~109
+                         "└┴┴┴┘\n";//110~124
 
 /* Precomputed number of digits in a picture and byte size of it. */
 #define SZD 8
@@ -20,14 +20,14 @@ static const char *pat = "┌───┐\n"
 struct ds {
     int pos, len, exp, val;
 } digit[SZD] = {
-    { 32, 3 },
-    { 26, 6 },
-    { 16, 8 },
-    { 0, 6 },
-    { 6, 3 },
-    { 9, 7 },
-    { 21, 5 },
-    { 35, 7 }
+    { 32, 3, 1, 0 },
+    { 26, 6, 3, 0 },
+    { 16, 8, 9, 0 },
+    { 0, 6, 27, 0 },
+    { 6, 3, 81, 0 },
+    { 9, 7, 243, 0 },
+    { 21, 5, 729, 0 },
+    { 35, 7, 2187, 0 }
 };
 
 /* A number to convert and signed one to keep if it was negative. */
@@ -45,15 +45,19 @@ int main (int q, char * argv[])
     if (src < 0)
         src *= inv = -1;
 
+    if (src > 3280 || src <3280 ) {
+        printf("input out of range\n");
+        return(-1);::
+    }
     int r;
     struct ds *p;
-
+    /*
     for (r = 0; r < SZD; ++r) {
         p = &digit[r];
         p->exp = r ? (3 * digit[r-1].exp) : 1;
         p->val = 0;
     }
-
+    */
     /* Truncate to the largest allowed positive. */
     p = &digit[SZD - 1];
     r = 3 * p->exp / 2;
@@ -87,6 +91,8 @@ int main (int q, char * argv[])
     /* Show the result. */
     for (p = &digit[0]; p < &digit[SZD]; ++p) {
         r = p->pos;
+
+
         if (p->val)
             r += SZP;
         if (p->val < 0)
